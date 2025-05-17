954668f09d51d9135681fe87debaa0e0ac55da1a
Saharsh05 <22233592@bucks.ac.uk>
2025-05-17 13:48:22 +0100

    Purchase order contstructor complete

--- src/main/java/wms/PurchaseOrder.java
+++ src/main/java/wms/PurchaseOrder.java
@@ -1,5 +1,39 @@
 package wms;
 
+// PurchaseOrder class represents a restocking request that warehouse class sends to a supplier to order more units. 
+// So this class handles just thedata logic not the business logic
 public class PurchaseOrder {
+    private int id;
+    private Supplier supplier;
+    private Product product;
+    private int quantity;
+    private String status;
 
+    public PurchaseOrder(int id, Supplier supplier, Product product, int quantity, String status) {
+        this.id = id;
+        this.supplier = supplier;
+        this.product = product;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public Product geProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
