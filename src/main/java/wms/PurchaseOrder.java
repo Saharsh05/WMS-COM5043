bedf9b00d8c71cdbae6a090beec30bbfe9ac247e
Saharsh05 <22233592@bucks.ac.uk>
2025-05-17 14:15:05 +0100

    Addition of a timestamp for orders which can be used to filter data in the future

--- src/main/java/wms/PurchaseOrder.java
+++ src/main/java/wms/PurchaseOrder.java
@@ -1,5 +1,7 @@
 package wms;
 
+import java.time.LocalDateTime;
+
 // PurchaseOrder class represents a restocking request that warehouse class sends to a supplier to order more units. 
 // So this class handles just thedata logic not the business logic
 public class PurchaseOrder {
@@ -8,13 +10,15 @@ public class PurchaseOrder {
     private Product product;
     private int quantity;
     private String status;
+    private LocalDateTime createdAt;
 
-    public PurchaseOrder(int id, Supplier supplier, Product product, int quantity, String status) {
+    public PurchaseOrder(int id, Supplier supplier, Product product, int quantity) {
         this.id = id;
         this.supplier = supplier;
         this.product = product;
         this.quantity = quantity;
-        this.status = status;
+        this.status = "Pending";
+        this.createdAt = LocalDateTime.now();
     }
 
     public int getId() {
@@ -25,12 +29,16 @@ public class PurchaseOrder {
         return supplier;
     }
 
-    public Product geProduct() {
+    public Product getProduct() {
         return product;
     }
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public LocalDateTime getCreatedAT() {
+        return createdAt;
     }
 
     public String getStatus() {
