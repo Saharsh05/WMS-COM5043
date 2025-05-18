877f0e3deeedd7672439478381b173b26c658aab
Saharsh05 <22233592@bucks.ac.uk>
2025-05-18 14:41:45 +0100

    added timestamo for customer orders

--- src/main/java/wms/CustomerOrder.java
+++ src/main/java/wms/CustomerOrder.java
@@ -1,19 +1,26 @@
 package wms;
 
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 // this class represents a customers order. it handles the data logic of what the customer buys. 
 public class CustomerOrder {
     private int id;
     private ArrayList<Product> products;
+    private LocalDateTime createdAt;
 
     public CustomerOrder(int id) {
         this.id = id;
         this.products = new ArrayList<>();
+        this.createdAt = LocalDateTime.now();
     }
 
     public int getId() {
         return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 
     public ArrayList<Product> getItems() {
