8f4003702dad6f4a350800bc5b878fc36779d132
Saharsh05 <22233592@bucks.ac.uk>
2025-05-21 10:43:03 +0100

    First initialisation of super class

---
+++ src/main/java/wms/Order.java
@@ -0,0 +1,16 @@
+package wms;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public abstract class Order {
+
+    private int id;
+    // private ArrayList<Product> history;
+    private LocalDateTime createdAt;
+
+    public Order(int id) {
+        this.id = id;
+        this.createdAt = LocalDateTime.now();
+    }
+}
