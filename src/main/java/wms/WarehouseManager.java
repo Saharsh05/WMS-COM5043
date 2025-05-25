fa0586203c943a238a373e99f40c22f9c8491a70
Saharsh05 <22233592@bucks.ac.uk>
2025-05-25 13:39:24 +0100

    added addSupplier() which focuses on the supplier logic in the warehouse manager class

--- src/main/java/wms/WarehouseManager.java
+++ src/main/java/wms/WarehouseManager.java
@@ -5,14 +5,19 @@ import java.util.ArrayList;
 
 public class WarehouseManager {
     private List<Product> products;
+    private List<Supplier> suppliers;
+    private List<PurchaseOrder> purchases;
 
     public WarehouseManager() {
         products = new ArrayList<>();
+        suppliers = new ArrayList<>();
+        purchases = new ArrayList<>();
     }
 
     public void addProduct(Product product) {
         if (product == null) {
             System.out.println("The product dosent exist");
+            return;
         }
 
         for (Product p : products) {
@@ -24,5 +29,22 @@ public class WarehouseManager {
 
         products.add(product);
         System.out.println("The product has been added");
+    }
+
+    public void addSupplier(Supplier supplier) {
+        if (supplier == null) {
+            System.out.println("The supplier dosent exist");
+            return;
+        }
+
+        for (Supplier s : suppliers) {
+            if (s.getId() == supplier.getId()) {
+                System.out.println("A supplier with this ID already exists");
+                return;
+            }
+        }
+
+        suppliers.add(supplier);
+        System.out.println("The supplier has been added");
     }
 }
