80dc39b43cedf81781528b0d954ab975282d5e90
Saharsh05 <22233592@bucks.ac.uk>
2025-05-25 14:01:41 +0100

    createPurchaseOrder() created creates a order with supplier product quanitity and Id and does check sto validate similiarties

--- src/main/java/wms/WarehouseManager.java
+++ src/main/java/wms/WarehouseManager.java
@@ -47,4 +47,33 @@ public class WarehouseManager {
         suppliers.add(supplier);
         System.out.println("The supplier has been added");
     }
+
+    public void createPurchaseOrder(int orderId, Supplier supplier, Product product, int quantity) {
+        if (supplier == null || quantity <= 0) {
+            System.out.println("One of supplier or quantity is incorrect");
+            return;
+        }
+
+        for (Supplier s : suppliers) {
+            if (s.getName().equals(supplier.getName())) {
+                System.out.println("The supplier already exsits");
+                return;
+            }
+        }
+
+        for (Product p : products) {
+            if (p.getName().equals(product.getName())) {
+                System.out.println("This product already exists");
+                return;
+            }
+        }
+
+        PurchaseOrder po = new PurchaseOrder(orderId, supplier, product, quantity);
+        purchases.add(po);
+
+        supplier.addOrder(po);
+
+        System.out.println("Purchase order created successfully");
+
+    }
 }
