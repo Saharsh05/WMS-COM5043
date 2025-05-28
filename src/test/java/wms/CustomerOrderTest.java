6f59f3f862e3761a6b75c3aba7db387a185ce441
Saharsh05 <22233592@bucks.ac.uk>
2025-05-28 18:59:47 +0000

    Fixed where @Test was and also tested to see how much of the class teh adddProduct test covered for CustomerOrder

--- src/test/java/wms/CustomerOrderTest.java
+++ src/test/java/wms/CustomerOrderTest.java
@@ -7,8 +7,9 @@ import wms.CustomerOrder;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@Test
 public class CustomerOrderTest {
+
+    @Test
     public void addProductCorrectValue(){
         CustomerOrder order = new CustomerOrder(301);
         Product product = new Product(201, "Strawberry",0, 1.99, 10);
