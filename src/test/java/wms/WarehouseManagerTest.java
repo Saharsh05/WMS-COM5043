5e36275812e79ffb0d7f570291f13e6bd508a185
Saharsh05 <22233592@bucks.ac.uk>
2025-05-30 00:17:07 +0100

    added test code to check for all branches in the method addPrudct in warehouse manager

---
+++ src/test/java/wms/WarehouseManagerTest.java
@@ -0,0 +1,52 @@
+package wms;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import src.main.java.wms.Product;
+import src.main.java.wms.WarehouseManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class WarehouseManagerTest {
+    @Test
+    public void addProductStoresNewProduct(){
+        WarehouseManager wManager = new WarehouseManager();
+        Product product = new Product(601, "Pencil", 10, 0.50, 2);
+        wManager.addProduct(product);
+
+        List<Product> storedProducts = wManager.getProducts();
+
+        assertEquals(1, storedProducts.size());
+        assertEquals(product, storedProduct.get(0));
+    }
+
+
+    @Test
+    public void addProductNullProductTest(){
+        WarehouseManager wManager = new WarehouseManager();
+        Product product = new Product(602, "Pen", 20, 2.50, 2);
+        wManager.addProduct(null);
+        List<Product> storeProducts = wManager.getProducts();
+
+        assertEquals(0, storeProducts.size());
+        
+    }
+
+    @Test
+    public void addProductCheckMultipleIDs(){
+        WarehouseManager wManager = new WarehouseManager();
+        Product product = new Product(603, "Ruler", 30, 0.35, 5);
+        Product product1 = new Product(603, "Ruler", 40, 0.45, 10);
+        
+        wManager.addProduct(product);
+        wManager.addProduct(product1);
+
+        List<Product> storeProducts = wManager.getProducts();
+
+        assertEquals(1, storeProducts.size());
+        assertEquals(product, storeProducts.get(0));
+
+    }
+}
